76d8aad960113430a6397b6fea3b14040458e906
IanDias10 <diasian8@hotmail.com>
2022-03-15 18:09:52 -0300

    Teste4

---
+++ teste4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Teste4
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            string name;
+            double altura;
+            int peso;
+            double imc;
+
+            Console.WriteLine("Qual o seu Nome: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Qual sua Altura: ");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Qual o seu Peso: ");
+            peso = Convert.ToInt32(Console.ReadLine());
+
+            imc = peso / (altura * altura);
+
+            Console.WriteLine(name + " " + altura + " " + peso + " " + imc);
+            Console.ReadKey();
+        }
+    }
+}
